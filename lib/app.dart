d908809923a95596dcdddaf49c05e44025f5a30e
Sifat Khan Joy <youremail@example.com>
2025-05-24 23:37:52 +0600

    Added full Flutter project files

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-
-// Import your custom screens/classes
 import 'package:ostad_batch10/satefull_class.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'flutter_class_1.dart';
 import 'flutter_class_2.dart';
 import 'from_login.dart';
+import 'module_9_class_3.dart';
+import 'module_11/class_1/alart.dart';
+import 'module_11/class_1/lifeCycle.dart';
+import 'module_11/class_2/calculatorApp.dart';
 import 'module_9_class_2.dart';
 import 'module_9_mediaQuery.dart';
 import 'module_9_todo.dart';
 
-/// The main entry point of the application.
-void main() {
-  runApp(const MyApp());
-}
-
-/// The root widget of the app.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -26,16 +21,16 @@ class MyApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (_, child) {
+      builder: (_ , child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'First Method',
-          // Use the ScreenUtil throughout your app including themes and text sizes
+          // You can use the library anywhere in the app even in theme
           home: child,
         );
       },
-      // Change the default screen here based on your test/demo needs
-      child: const ModuleNineClassTwo(),
+      child: const CalculatorApp(),
     );
   }
+
 }
--- lib/module_9_class_2.dart
+++ lib/module_9_class_2.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 class ModuleNineClassTwo extends StatefulWidget {
   const ModuleNineClassTwo({super.key});
 
